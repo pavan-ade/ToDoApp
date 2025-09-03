d08901484780a260d850bd4f0bf96d9a8ee1a3d2
Pavan Ade <126013713+pavan-ade@users.noreply.github.com>
2025-09-04 01:39:23 +0530

    [pavan-ade] : response in frontend

--- RedexToolToDo/src/pages/layout/Layout.jsx
+++ RedexToolToDo/src/pages/layout/Layout.jsx
@@ -6,9 +6,9 @@ import SideBar from "../sidebar/SideBar";
 const Layout = () => {
   return (
     <>
-      <div className="h-screen flex gap-1">
+      <div className="h-screen flex">
         <SideBar />
-        <div className=" flex-1 overflow-y-auto bg-gray-100 dark:bg-gray-600">
+        <div className="flex-1 overflow-y-auto bg-gray-100 dark:bg-gray-600">
           <div className="sticky py-2 top-0 w-full bg-white dark:bg-gray-600 shadow-md z-50">
             <h1 className="md:text-[50px] font-bold text-center xs:text-[30px]">Todo App</h1>
             <ThemeContent />
